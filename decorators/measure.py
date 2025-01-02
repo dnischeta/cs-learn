0085cb2c2b3fd34ff11e63e379dbea6e66a77e10
Dmitry Nischeta <dnishcheta@gmail.com>
2025-01-02 22:30:43 +0500

    Add measure decorator

---
+++ decorators/measure.py
@@ -0,0 +1,19 @@
+from time import perf_counter
+
+def measure(f: callable):
+    def withMeasure(*args):
+        start = perf_counter()
+        r = f(*args)
+        end = perf_counter()
+        print(f"DEBUG: {f.__name__} took {'{0:0.2f}'.format((end-start) * 1000)}ms")
+        return r
+    return withMeasure
+        
+@measure
+def sum():
+    r = 0
+    for i in range(100000):
+        r += i
+    return r
+
+print(sum())
